25f70421366001d549c255289835bf6e25a30f31
Phan Ho Ha Phuong <elvis.phan@finos.asia>
2020-04-01 01:06:05 +0700

    inital commit

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h3 className="App-title">Fib calculator</h3>
+            <h3 className="App-title">Fib calculator version 2</h3>
             <Link to="/">Home</Link><Link to="/otherpage">Other Page</Link>
           </header>
           <div>
--- server/index.js
+++ server/index.js
@@ -38,19 +38,19 @@ app.get('/', (req, res) => {
     res.send ('Hi');
 });
 
-app.get('/values/all', async (req, res) => {
+app.get('/api/values/all', async (req, res) => {
     const values = await pgClient.query('SELECT * FROM values');
 
     res.send(values.rows);
 });
 
-app.get('/values/current', async (req, res) => {
+app.get('/api/values/current', async (req, res) => {
     redisClient.hgetall('values', (err, values) => {
         res.send(values);
     });
 });
 
-app.post('/values', async (req, res) => {
+app.post('/api/values', async (req, res) => {
     const index = req.body.index;
 
     if (parseInt(index) > 40) {
